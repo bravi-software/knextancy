fd3a00c865727c76d0e82232eba1476879e8cfdc
Paulo Ragonha <paulo@ragonha.me>
2014-10-29 18:30:54 -0200

    exposes connect middleware to populate knex

--- index.js
+++
@@ -1,8 +0,0 @@
-// setup ES6 Proxy shim
-require('harmony-reflect');
-
-
-module.exports = function (baseKnex, tenant) {
-  var proxyKnex = knex({ __client__: proxyClientTenant(baseKnex.client, tenant)});
-  return proxyKnex.migrate.latest();
-};
