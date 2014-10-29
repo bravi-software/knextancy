8740b761741acbdf5df9466846ce8bf1b8224383
Paulo Ragonha <paulo@ragonha.me>
2014-10-29 18:02:38 -0200

    made it a lib

--- index.js
+++ index.js
@@ -2,27 +2,7 @@
 require('harmony-reflect');
 
 
-var knex = require('knex'),
-    proxyClientTenant = require('./lib/proxy-client-tenant');
-
-
-var baseKnex = knex(require('./knexfile').development);
-
-
-var proxyKney = knex({ __client__: proxyClientTenant(baseKnex.client, 'tenant-1')});
-
-
-proxyKney.migrate.latest().then(function () {
-  console.log('migrated');
-
-  proxyKney('$tenant:users').where({
-    name: 'Test'
-  }).select('name').then(function (users) {
-    console.log(users);
-  }, function (err) {
-    console.log(err);
-  });
-});
-
-
-console.log('started');
+module.exports = function (baseKnex, tenant) {
+  var proxyKnex = knex({ __client__: proxyClientTenant(baseKnex.client, tenant)});
+  return proxyKnex.migrate.latest();
+};
