e645736dd946304fcd3110f18e201980dda583b4
Max Claus Nunes <maxcnunes@gmail.com>
2015-07-16 00:32:40 -0300

    fixes tests was dropping the tables but should only truncate them

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -6,15 +6,26 @@ var fs = require('fs'),
     path = require('path');
 
 
-var removeTablesSQL = fs.readFileSync(path.join(__dirname, './spec-helper.sql'), { encoding: 'utf8' });
+var truncateTablesSQL = fs.readFileSync(path.join(__dirname, './spec-helper.sql'), { encoding: 'utf8' });
 
 
 var knex = require('knex')(require('./fixtures/knexfile').test);
 
 
 beforeEach(function() {
-  return knex.raw(removeTablesSQL);
+  return truncateAllTables();
 });
+
+
+function truncateAllTables () {
+  return knex.raw(truncateTablesSQL).then(function (sql) {
+    sql = sql[0].map(function (sqlQuery) {
+      return sqlQuery.trucate_table_cmd;
+    }).join('');
+
+    return knex.raw(sql);
+  });
+}
 
 
 module.exports = {
--- spec/spec-helper.sql
+++ spec/spec-helper.sql
@@ -1,16 +1,10 @@
--- script that removes all tables from a database
--- source: http://stackoverflow.com/questions/12403662/how-to-drop-all-mysql-tables-from-the-command-line/18625545#18625545
+-- script that truncate all tables from a database
 
-SET FOREIGN_KEY_CHECKS = 0;
-SET GROUP_CONCAT_MAX_LEN=32768;
-SET @tables = NULL;
-SELECT GROUP_CONCAT('`', table_name, '`') INTO @tables
-  FROM information_schema.tables
-  WHERE table_schema = (SELECT DATABASE());
-SELECT IFNULL(@tables,'dummy') INTO @tables;
-
-SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
-PREPARE stmt FROM @tables;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
-SET FOREIGN_KEY_CHECKS = 1;
+SELECT 'SET FOREIGN_KEY_CHECKS = 0;' trucate_table_cmd
+UNION
+SELECT Concat('TRUNCATE TABLE `' ,table_schema, '`.`', TABLE_NAME, '`;') trucate_table_cmd
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE table_schema = (SELECT database())
+    AND TABLE_NAME NOT LIKE '%knex_migrations'
+UNION
+SELECT 'SET FOREIGN_KEY_CHECKS = 1;' trucate_table_cmd
