89f2c8afa830093c7f953ffa816dc1b0409b2673
Max Claus Nunes <maxcnunes@gmail.com>
2015-09-23 00:51:09 -0300

    Simplify configuration using shorter env variables

--- spec/fixtures/knexfile.js
+++ spec/fixtures/knexfile.js
@@ -1,13 +1,16 @@
-module.exports = {
+import url from 'url';
 
+
+export default {
   test: {
     client: 'mysql',
     connection: {
       multipleStatements: true,
-      host: process.env.MYSQL_PORT_3306_TCP_ADDR,
-      user: process.env.MYSQL_ENV_MYSQL_USER,
-      password: process.env.MYSQL_ENV_MYSQL_PASSWORD,
-      database: process.env.MYSQL_ENV_MYSQL_DATABASE,
+      host: url.parse(process.env.DB_PORT).hostname,
+      port: url.parse(process.env.DB_PORT).port,
+      user: process.env.DB_USER || process.env.DB_ENV_MYSQL_USER,
+      password: process.env.DB_PASSWORD || process.env.DB_ENV_MYSQL_PASSWORD,
+      database: process.env.DB_DATABASE || process.env.DB_ENV_MYSQL_DATABASE,
     },
     migrations: {
       directory: './spec/fixtures/db/migrations',
